12b009d1de19a2d67906012f2d318f1b1af26686
svveden <55561624+svveden@users.noreply.github.com>
2022-01-24 12:14:33 -0600

    Chapter 2 Question 1 added

---
+++ c2q1.c
@@ -0,0 +1,13 @@
+/* 01/24/2022 */
+
+#include <stdio.h>
+
+int main(void){
+	printf("%25c\n", '*');
+	printf("%22c\n", '*');
+	printf("%19c\n", '*');
+	printf("%7c%10c\n",'*', '*');
+	printf("%9c%5c\n", '*', '*');
+	printf("%11c\n\n", '*');
+	return 0;
+}
