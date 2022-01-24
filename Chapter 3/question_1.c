22dd6cffd1a52bdedd8b33cee24990f58da7a3eb
svveden <55561624+svveden@users.noreply.github.com>
2022-01-24 13:29:27 -0600

    added question 1 chp 3

---
+++ Chapter 3/question_1.c
@@ -0,0 +1,14 @@
+/* 1/24/2022 */
+/* This program takes in a date in format mm/dd/yyyy and converts it to yyyymmdd. */
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){
+	int month;
+	int day;
+	int year;
+	printf("Enter a date (mm/dd/yyyy): ");
+	scanf("%d/%d/%d",&month,&day,&year);
+	printf("You entered the date %d%.2d%d\n",year,month,day);
+	return 0;
+}
