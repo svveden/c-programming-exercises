25e9efa56f9447c1f6d4c041311491779ee55bd2
svveden <55561624+svveden@users.noreply.github.com>
2022-01-25 10:04:11 -0600

    added question 1

---
+++ Chapter 4/question_1.c
@@ -0,0 +1,12 @@
+/* 01/25/2022 */
+/* This program takes in a two digit number and outputs the reversal of that number */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){
+	int num;
+	printf("Enter a two digit number: ");
+	scanf("%d", &num);
+	printf("The reversal is: %d%d\n",num%10,num/10);
+}
