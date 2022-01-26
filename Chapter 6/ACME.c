36f8e52281a42698ff7d41f71550cdf6893b75dd
svveden <55561624+svveden@users.noreply.github.com>
2022-01-26 11:45:34 -0600

    Create ACME.c

---
+++ Chapter 6/ACME.c
@@ -0,0 +1,42 @@
+/* 01/26/2022 */
+/* This program is a balancing checkbook application */
+#include <stdio.h>
+
+int main(void){
+	float credit = 0.0f;
+	float debit = 0.0f;
+	float balance = 0.0f;
+	int command = 0;
+
+	for(;;){
+		printf("*** ACME checkbook-balancing program ***\nCommands: 0=clear, 1=credit, 2=debit, 3=balance, 4=exit\n");
+		printf("Enter command: ");
+		scanf("%d", &command);
+		switch(command){
+			case 0:
+				balance = 0.0f;
+				printf("Balance cleared, current balance: %.2f\n", balance);
+				break;
+			case 1: 
+				printf("Enter amount of credit: ");
+				scanf("%f", &credit);
+				balance += credit;
+				break;
+			case 2:
+				printf("Enter amount of debit: ");
+				scanf("%f", &debit);
+				balance -= debit;
+				break;
+			case 3:
+				printf("Current balance: %.2f\n", balance);
+				break;
+			case 4:
+				printf("Exiting, Thank you for using ACME\n");
+				return 0;
+			default: printf("Invalid command.");
+				break;
+		}
+		printf("\n");
+	}
+	return 0;
+}
