b0ee311717aa91ae21e70b59343ed32aab7d1bcb
svveden <55561624+svveden@users.noreply.github.com>
2022-01-26 11:25:44 -0600

    added for loop version

--- Chapter 6/table_of_squares.c
+++ Chapter 6/table_of_squares.c
@@ -13,3 +13,17 @@ int main(void){
 	}
 	return 0;
 }
+
+// /* 01/26/2022 */
+// /* This program asks for a number and then outputs a table of squares looping up to that number (for loop) */
+// #include <stdio.h>
+
+// int main(void){
+// 	int entries;
+// 	printf("This program prints a table of squares.\nEnter number of entries in table: ");
+// 	scanf("%d", &entries);
+// 	for(int i=1;i<=entries;i++){
+// 		printf("\t%d\t%d\n",i,i*i);
+// 	}
+// 	return 0;
+// }
