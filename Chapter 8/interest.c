23ae2981719d9163e12be588e65f3d5b4dd5e093
svveden <55561624+svveden@users.noreply.github.com>
2022-01-28 20:40:32 -0600

    Create interest.c

---
+++ Chapter 8/interest.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#define YEARS 5
+
+int main(void){
+	int year, x = 0;
+	float interest;
+	float cash = 100.00f;
+	float rate[YEARS] = {0};
+	printf("Enter interest rate: ");
+	scanf("%f", &interest);
+	printf("Enter the number of years: ");
+	scanf("%d", &year);
+	printf("Years \t %.0f %% \t%.0f %% \t%.0f %% \t%.0f %% \t%.0f %% \n",interest,interest+1,interest+2,interest+1,interest+4);
+	interest /= 100;
+	printf("1\t");
+	for(int i = 0; i < YEARS; i++){
+		rate[i] = cash + cash*interest;
+		interest+=0.01;
+		printf("%.2f\t",rate[i]);
+	}
+	printf("\n");
+	while(x < year-1){
+		interest-=0.05;
+		printf("%d\t", x+2);
+		for(int j = 0; j < YEARS; j++){
+			rate[j] = rate[j]*interest + rate[j];
+			interest+=0.01;
+			printf("%.2f\t", rate[j]);
+		}
+		x+=1;
+		printf("\n");
+	}
+
+	return 0;
+}
