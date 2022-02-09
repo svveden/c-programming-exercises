204ff4a09615386025781ef300b4b2c01af2ccef
svveden <55561624+svveden@users.noreply.github.com>
2022-02-09 09:31:43 -0600

    Create question_6.c

---
+++ Chapter 8/question_6.c
@@ -0,0 +1,26 @@
+/*	02/09/2022 */
+/* This program declares a multidiminsional array for keeping track of digital number output */
+
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(void){
+	int num;
+	const int segments[10][7] = {{1, 1, 1, 1, 1, 1, 0},
+								 {0, 1, 1, 0, 0, 0, 0},
+								 {1, 1, 0, 1, 1, 0, 1},
+								 {1, 1, 1, 1, 0, 0, 1},
+								 {0, 1, 1, 0, 0, 1, 1},
+								 {1, 0, 1, 1, 0, 1, 1},
+								 {1, 0, 1, 1, 1, 1, 1},
+								 {1, 1, 1, 0, 0, 0, 0},
+								 {1, 1, 1, 1, 1, 1, 1},
+								 {1, 1, 1, 1, 0, 1, 1}};
+	printf("Enter a number 0 - 9: ");
+	scanf("%d", &num);
+	for(int i = 0; i<7; i++){
+		printf("%d ", segments[num][i]);
+	}
+	printf("\n");
+	return 0;
+}
