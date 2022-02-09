35190ef0ee2721bbaae1bddac0e441b538599d1d
svveden <55561624+svveden@users.noreply.github.com>
2022-02-09 14:55:35 -0600

    Update question_8.c

--- Chapter 8/question_8.c
+++ Chapter 8/question_8.c
@@ -1,42 +1,40 @@
 /* 02/09/2022 */
-/* This program takes in a 5 students grades for 5 quizess then outputs requested answers. */
+/* This program takes in a 5 students grades for 5 quizzes then outputs requested answers. */
 
 #include <stdio.h>
 #include <ctype.h>
+#define STUDENTS 5
+#define QUIZZES 5
 
 int main(void){
-	int ints[5][5] = {0};
+	int grades[STUDENTS][QUIZZES] = {0};
 	int row = 0, column = 0, high = 0, low = 101;
-	printf("Enter quizzes for student 1: ");
-	scanf("%d %d %d %d %d", &ints[0][0],&ints[0][1],&ints[0][2],&ints[0][3],&ints[0][4]);
-	printf("Enter quizzes for student 2: ");
-	scanf("%d %d %d %d %d", &ints[1][0],&ints[1][1],&ints[1][2],&ints[1][3],&ints[1][4]);
-	printf("Enter quizzes for student 3: ");
-	scanf("%d %d %d %d %d", &ints[2][0],&ints[2][1],&ints[2][2],&ints[2][3],&ints[2][4]);
-	printf("Enter quizzes for student 4: ");
-	scanf("%d %d %d %d %d", &ints[3][0],&ints[3][1],&ints[3][2],&ints[3][3],&ints[3][4]);
-	printf("Enter quizzes for student 5: ");
-	scanf("%d %d %d %d %d", &ints[4][0],&ints[4][1],&ints[4][2],&ints[4][3],&ints[4][4]);
-	printf("\n");
-	for(int i = 0; i < 5; i++){
-		row = 0;
-		for(int j = 0; j< 5; j++){
-			row += ints[i][j];
+	for(int i = 0; i < STUDENTS; i++){
+		printf("Enter quizzes for student %d: ", i);
+		for(int j = 0; j < QUIZZES; j++){
+			scanf("%d", &grades[i][j]);
 		}
-		printf("Student %d totals: %d\nStudent %d average: %d\n\n", i, row, i, row/5);
 	}
-	for(int i = 0; i < 5; i++){
+	printf("\n");
+	for(int i = 0; i < STUDENTS; i++){
+		row = 0;
+		for(int j = 0; j< QUIZZES; j++){
+			row += grades[i][j];
+		}
+		printf("Student %d totals: %d\nStudent %d average: %d\n\n", i, row, i, row/STUDENTS);
+	}
+	for(int i = 0; i < STUDENTS; i++){
 		column = 0;
-		for(int j = 0; j< 5; j++){
-			column += ints[j][i];
-			if(high < ints[j][i]){
-				high = ints[j][i];
-			} else if(low > ints[i][j]){
-				low = ints[i][j];
+		for(int j = 0; j < QUIZZES; j++){
+			column += grades[j][i];
+			if(high < grades[j][i]){
+				high = grades[j][i];
+			} else if(low > grades[i][j]){
+				low = grades[i][j];
 			}
 		}
 	}
-	printf("Quiz average: %d\n", column/5);
+	printf("Quiz average: %d\n", column/QUIZZES);
 	printf("Quiz high: %d\n", high);
 	printf("Quiz low: %d\n ", low);
 	return 0;
