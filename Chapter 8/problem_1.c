ea2b699c49f6aa956d181773286db1f09ce57555
svveden <55561624+svveden@users.noreply.github.com>
2022-02-09 09:13:18 -0600

    Create problem_1.c

---
+++ Chapter 8/problem_1.c
@@ -0,0 +1,10 @@
+/*	02/09/2022 */
+/* This program declares an array of size 7 of bools, making the first and last true. */
+
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(void){
+	bool weekend[7] = {true, false, false, false, false, false, true};
+	return 0;
+}
