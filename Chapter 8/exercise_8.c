dff0ccd2ebc1ced271d2763911443561aa0f51e1
svveden <55561624+svveden@users.noreply.github.com>
2022-02-09 09:39:55 -0600

    Create exercise_8.c

---
+++ Chapter 8/exercise_8.c
@@ -0,0 +1,10 @@
+/*	02/09/2022 */
+/* This program declares a multidiminsional array for keeping track of hourly temperatures for a month */
+
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(void){
+	double temperature_readings[30][24] = {0};
+	return 0;
+}
