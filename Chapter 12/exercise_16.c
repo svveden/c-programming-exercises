965ca18e6d60ce7e1d73b72f8b761b2896962230
svveden <55561624+svveden@users.noreply.github.com>
2022-02-18 20:57:51 -0600

    Create exercise_16.c

---
+++ Chapter 12/exercise_16.c
@@ -0,0 +1,11 @@
+/* 02/18/2022 */
+/* Searches array for largest temp */
+#include <stdio.h>
+
+int main(void){
+	int temperatures[7][24];
+	int *p;
+	for(p = temperatures[i]; p < temperatures[7][24]; p++){
+		find_largest(p, 24);
+	}
+}
