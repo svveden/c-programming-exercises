322e29dffd12f150129d978a21b1c793ff1b8a49
Jared Moise <55561624+svveden@users.noreply.github.com>
2022-02-25 10:34:05 -0600

    Create calc.c

---
+++ Chapter 15/question_5/calc.c
@@ -0,0 +1,14 @@
+/* 02/16/2022 */
+/* Stack for RPN */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "stack.h"
+
+int main(void){
+	while(1){
+		menu();
+	}
+	return 0;
+}
