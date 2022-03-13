7f29b5602aa758e3530db27efff64123ba160cf8
svveden <55561624+svveden@users.noreply.github.com>
2022-03-12 22:46:07 -0600

    prog_8

---
+++ Chapter 22/programming_projects/project_08/database_functions.c
@@ -0,0 +1,96 @@
+/* 02/26/2022 */
+/* Parts Database Functions */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <strings.h>
+#include <ctype.h>
+
+#ifndef DATABASE_H
+#define DATABASE_H
+#include "database_functions.h"
+#endif
+
+
+struct part{
+	int number;
+	char name[MAX_NAME_LENGTH+1];
+	int in_stock;
+};
+
+struct part inventory[100];
+int inventory_position = 0;
+
+int read_line(char str[], int n){
+	char c;
+	int i = 0;
+	while(isspace(c = getchar()))
+		;
+	while(c != EOF && c != '\n'){
+		if(i < n){
+			str[i++] = c;
+		} c = getchar();
+	} str[i] = '\0';
+	return i;
+}
+
+void insert(int number, char name[MAX_NAME_LENGTH+1], int in_stock){
+	inventory[inventory_position].number = number;
+	inventory[inventory_position].in_stock = in_stock;
+	for(int i = 0; i < strlen(name); i++){
+		inventory[inventory_position].name[i] = name[i];
+	}
+	inventory_position++;
+}
+
+void search_item(int number){
+	for(int i = inventory_position; i >= 0; i--){
+		if(inventory[i].number == number){
+			printf("Name: %s\n", inventory[i].name);
+			printf("Amount in stock: %d\n", inventory[i].in_stock);
+			return;
+		} 
+	} printf("Item not found!");
+}
+
+void update_item(int number, int update){
+	for(int i = inventory_position; i >= 0; i--){
+		if(inventory[i].number == number){
+				inventory[i].in_stock += update;
+				return;
+			}
+		}
+	}
+
+void print_inventory(void){
+	printf("Part number %15s %20s\n", "Part name", "Amount in stock");
+	for(int i = 0; i < inventory_position; i++){
+		printf("%-18d%-15s%d\n",inventory[i].number,inventory[i].name,inventory[i].in_stock);
+	}
+	return;
+}
+
+void dump_inventory(void){
+	FILE *fp;
+	if((fp = fopen("inventory.dat", "wb+")) != NULL){
+		fwrite(inventory, sizeof(struct part), inventory_position, fp);
+	    fclose(fp);
+	}else{
+		fprintf(stderr, "ERROR DUMPING FILE!!\n");
+		exit(EXIT_FAILURE);
+	}
+	return;
+}
+
+void restore_inventory(void){
+	FILE *fp;
+	if((fp = fopen("inventory.dat", "rb")) != NULL){
+		inventory_position = fread(inventory, sizeof(struct part), sizeof(inventory)/sizeof(inventory[0]), fp);
+	} else{
+		fprintf(stderr, "ERROR OPENING FILE, DOES IT EXIST?\n");
+		exit(EXIT_FAILURE);
+	}
+	return;
+}
+
+
